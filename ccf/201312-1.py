e04b459b1cf1102ea49ba8f76c46f28326261d0c
emuqi <invincible9257@gmail.com>
2019-07-21 21:37:03 +0800

    ccf 201312-1

---
+++ ccf/201312-1.py
@@ -0,0 +1,19 @@
+#试题编号：	201312-1
+#试题名称：	出现次数最多的数
+
+n = int(input())
+s = input()
+
+list_of_num = s.split(" ")
+max = float("-inf")
+result = float("inf")
+for temp in list_of_num:
+    i = list_of_num.count(temp)
+    if i > max:
+        max = i
+
+for temp in list_of_num:
+    if list_of_num.count(temp) == max and result > int(temp):
+        result = int(temp)
+
+print(result)
