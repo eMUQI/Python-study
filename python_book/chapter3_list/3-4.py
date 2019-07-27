c146364fd04e5d97d379f67690caad7239e08cf4
emuqi <invincible9257@gmail.com>
2019-07-27 17:56:07 +0800

    book3.2

---
+++ python_book/chapter3_list/3-4.py
@@ -0,0 +1,3 @@
+list_of_person = ["Person A", "Person B", "Person C"]
+for temp in list_of_person:
+    print("Dear " + temp + ",可以和我一起共进午餐吗？")
--- python_book/chapter3_list/bicycles.py
+++
@@ -1,8 +0,0 @@
-bicycles = ['trek','cannondale','redline','specialized']
-print(bicycles)
-print(bicycles[0])
-print(bicycles[0].title())
-print(bicycles[-1].title())
-
-message = "My first bicycle was a " + bicycles[0].title() + "."
-print(message)
